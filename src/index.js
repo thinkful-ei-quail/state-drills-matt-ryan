6eb0110157ea28bb5e32406ffc430be0b9f85de0
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-07-22 13:19:05 -0700

    Adjusted content

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import Accordian from './state-drills/Accordian';
 const sections = [
   {
     title: 'Section 1',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    content: 'Never gonna give you up.',
   },
   {
     title: 'Section 2',
-    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+    content: 'Never gonna let you down.',
   },
   {
     title: 'Section 3',
-    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+    content: 'Never gonna hurt you.',
   },
 ]
 
